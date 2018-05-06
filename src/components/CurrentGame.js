a1c19b3e326cf83ff65b8fcbb0ed0521edbd94da
NEPTUNE\jeff <jeff@martinzone.com>
2018-05-06 11:07:20 -0500

    alpha 2

--- src/components/CurrentGame.js
+++ src/components/CurrentGame.js
@@ -20,17 +20,16 @@ class CurrentGame extends React.Component {
         {
             return (
                 <div>
-                    <ShowScores players={this.props.app.state.players}/>
                     <div className="court">
                         <div className="sideTitle">{game.InSide}</div>
                         <div className="side" onClick={() => this.props.app.winner(cur,true)}>  
-                            <div className={"server player" + inWinner}>{game.Server.Name}</div>
-                            <div className={"player"  + inWinner}>{game.TeamMate.Name}</div>
+                            <div className={"server player" + inWinner}>{game.Server.Name} ({game.Server.Score})</div>
+                            <div className={"player"  + inWinner}>{game.TeamMate.Name} ({game.TeamMate.Score})</div>
                         </div>
                         <div className="net">Game {this.props.app.state.CurrentGame+1}</div>   
                         <div className="side" onClick={() => this.props.app.winner(cur,false)}>
-                            <div className={"player"  + outWinner}>{game.Opp1.Name}</div>
-                            <div className={"player"  + outWinner}>{game.Opp2.Name}</div>
+                            <div className={"player"  + outWinner}>{game.Opp1.Name} ({game.Opp1.Score})</div>
+                            <div className={"player"  + outWinner}>{game.Opp2.Name} ({game.Opp2.Score})</div>
                         </div>  
                         <div className="sideTitle">{game.OutSide}</div>                               
                     </div>
@@ -48,17 +47,16 @@ class CurrentGame extends React.Component {
         else {
             return (
                 <div>
-                    <ShowScores players={this.props.app.state.players}/>
                     <div className="court">
                         <div className="sideTitle">{game.OutSide}</div>
                         <div className="side" onClick={() => this.setWinner(cur,false)}>  
-                        <div className={"player"  + outWinner}>{game.Opp1.Name}</div>
-                            <div className={"player"  + outWinner}>{game.Opp2.Name}</div>
+                        <div className={"player"  + outWinner}>{game.Opp1.Name} ({game.Opp1.Score})</div>
+                            <div className={"player"  + outWinner}>{game.Opp2.Name} ({game.Opp2.Score})</div>
                         </div>
                         <div className="net">Game {this.props.app.state.CurrentGame+1}</div>   
                         <div className="side" onClick={() => this.setWinner(cur,true)}>
-                            <div className={"player server"  + inWinner}>{game.Server.Name}</div>
-                            <div className={"player"  + inWinner}>{game.TeamMate.Name}</div>
+                            <div className={"player server"  + inWinner}>{game.Server.Name} ({game.Server.Score})</div>
+                            <div className={"player"  + inWinner}>{game.TeamMate.Name} ({game.TeamMate.Score})</div>
                         </div>  
                         <div className="sideTitle">{game.InSide}</div>                               
                     </div>
